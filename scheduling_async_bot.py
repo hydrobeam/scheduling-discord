66c2d88d932136dac13535116a8f8ca776386976
laith <laithbahodi@gmail.com>
2021-02-06 19:32:11 -0500

    added remove_index, config file, timezone awarness in listener

--- scheduling_async_bot.py
+++ scheduling_async_bot.py
@@ -11,68 +11,26 @@ from pymongo import MongoClient
 from datetime import timedelta, datetime
 from pytz import timezone
 from uuid import uuid4
+
+import configparser
 import logging
 
-from utility_file import format_dt, short_dt,send_message
+from utility_file import format_dt, short_dt, send_message
 
 from pprint import pprint
 
 # TODO: prioritize email in presenation
-# TODO: make the config stuff a separete file
 # TODO: inndex removals
-# TODO: if name == 'main' ?
 # TODO: Interval message redundant? evaluate
 # TODO: bdtweentwo tiems tomorow
 
 # Initialization stuff
-
-# coloredlogs.install()
 client = discord.Client()
 slash = SlashCommand(client, auto_register=True, auto_delete=True)
-mongoclient = MongoClient("mongodb+srv://BotOwner:M26ToshtFDBuT6SY@schedule-bot.c6ats.mongodb.net/discord"
-                          "?retryWrites=true&w=majority")
-
-db = mongoclient.discord
-
 guild_ids = [687499582459871242, 748887953497129052, 677353989632950273]
 
-jobstores = {
-    'default': MongoDBJobStore(client=mongoclient, collection="scheduled-job")
-}
-mainsched = AsyncIOScheduler(jobstores=jobstores)
-mainsched.start()
 
-
-def jobitem_removed(event):
-    # the listener checks for when a job is removed, or executed and removes it from 'active jobs'
-
-    user_id = int(event.job_id.split('user')[1])
-    job = mainsched.get_job(event.job_id)
-    current_time = datetime.now(timezone('America/New_York'))
-
-    try:
-        # if end_date exists, check if end_date has already passed, then remove the job if True. if no end_date,
-        # or end_date not passed, execute the function
-        if x := job.trigger.end_date:
-            if current_time >= x:
-                # the end_date has passed, remove hte job
-                pass
-            else:
-                # the end_date has note yet passed, do not remove the job
-                return
-        else:
-            # there is no end_date, the job never ends
-            return
-    except AttributeError:
-        # this is a date job, proceed as normal remove the job
-        pass
-    db.bot_usage.find_one_and_update({'user id': user_id},
-                                     {'$pull': {'active jobs': event.job_id}})
-    logging.info(f"event: {event}, listener activated")
-
-
-mainsched.add_listener(jobitem_removed, EVENT_JOB_EXECUTED | EVENT_JOB_REMOVED)
-
+# coloredlogs.install()
 
 # serious commands
 
@@ -134,7 +92,6 @@ async def date_message(ctx, message, time_of_day, day_of_month=None, month_of_ye
     if year is None:
         year = datetime.now(user_tz).year
 
-
     # check if am or pm are used, set lower and remove spaces: 9:08 pm == 9:08pm
     time_of_day = time_of_day.lower().replace(' ', '')
     if "am" in time_of_day or "pm" in time_of_day:
@@ -142,7 +99,7 @@ async def date_message(ctx, message, time_of_day, day_of_month=None, month_of_ye
     else:
         time = datetime.strptime(time_of_day, '%H:%M')
 
-    # define the time of delivery, causews my issue
+    # define the time of delivery,
     planned_time = user_tz.localize(datetime(year, month_of_year, day_of_month, time.hour, time.minute))
 
     # check if in the past
@@ -196,6 +153,7 @@ async def time_from_now(ctx, message, duration):
 
     await ctx.send(content=f"⏰ Message: **{message}** - scheduled for *{format_dt(planned_time)}*  ")
 
+
 @slash.slash(name="interval-message", description="repeat a message for a specified duration/interval",
              guild_ids=guild_ids,
              options=[
@@ -238,13 +196,14 @@ async def interval(ctx, duration, increment, message):
 
     end_date = datetime.now(user_tz) + timedelta(minutes=duration)
     mainsched.add_job(send_message, 'interval', minutes=increment, end_date=end_date,
-                      args=(message, doc['contact information']), misfire_grace_time=500, replace_existing=True, id=id_, timezone=user_tz)
+                      args=(message, doc['contact information']), misfire_grace_time=500, replace_existing=True, id=id_,
+                      timezone=user_tz)
 
     # add to  active jobs
     db.bot_usage.find_one_and_update({'user id': user_id},
                                      {'$push': {'active jobs': id_}})
 
-    await ctx.send(content=f"Message created: {message} ", complete_hidden=True)
+    await ctx.send(content=f"⏰ Message: **{message}** - to end at *{format_dt(end_date)}*")
 
 
 @slash.slash(name="define-self", description="Initialize your details", guild_ids=guild_ids,
@@ -279,31 +238,25 @@ async def define_self(ctx, contact_info, tz='America/New_York'):
         {"$setOnInsert": {'active jobs': []}},
         upsert=True
     )
-    await ctx.send(content=f"Information registered: {contact_info}, Timezone: {tz}", complete_hidden=True)
+    await ctx.send(content=f"**Information registered**: {contact_info}, **Timezone**: {tz}", complete_hidden=True)
 
 
 @slash.slash(name="daily-reminder", description="Set a daily reminder", guild_ids=guild_ids,
              options=[
                  manage_commands.create_option(
-                     name="hour",
-                     description="specify hour, 24h clock",
-                     option_type=4,
-                     required=True
-                 ),
-                 manage_commands.create_option(
-                     name="minute",
-                     description="specify minute",
-                     option_type=4,
+                     name="time",
+                     description="time of day - HH:MM",
+                     option_type=3,
                      required=True
                  ),
                  manage_commands.create_option(
                      name="message",
-                     description="specify message, uses message matrix value if empty",
+                     description="specify message",
                      option_type=3,
                      required=True
                  )
              ])
-async def daily_reminder(ctx, hour, minute, message):
+async def daily_reminder(ctx, time_of_day, message):
     # init data
     try:
         user_id, id_, doc, user_tz = basic_init(ctx)
@@ -311,13 +264,19 @@ async def daily_reminder(ctx, hour, minute, message):
         await ctx.send(content=f"Please register your information with 'define-self'")
         return
 
+    time_of_day = time_of_day.lower().replace(' ', '')
+    if "am" in time_of_day or "pm" in time_of_day:
+        time = datetime.strptime(time_of_day, '%I:%M%p')
+    else:
+        time = datetime.strptime(time_of_day, '%H:%M')
+
     # initialize the class
-    mainsched.add_job(send_message, 'cron', args=(message, doc['contact information']), hour=hour,
-                      minute=minute, misfire_grace_time=500, replace_existing=True, id=id_, timezone=user_tz)
+    mainsched.add_job(send_message, 'cron', args=(message, doc['contact information']), hour=time.hour,
+                      minute=time.minute, misfire_grace_time=500, replace_existing=True, id=id_, timezone=user_tz)
     db.bot_usage.find_one_and_update({'user id': user_id},
                                      {'$push': {'active jobs': id_}})
 
-    await ctx.send(content=f"Schedule created: for {hour}:{minute} send {message}")
+    await ctx.send(content=f"⏰ Message: **{message}** - to be sent at *{short_dt(time)}*")
 
 
 @slash.slash(name="between-two-times", description='send messages at an interval between two times throughout the day',
@@ -378,7 +337,7 @@ async def between_times(ctx, time_1, time_2, interval, message, repeating):
     time_1 = today.replace(hour=time_1.hour, minute=time_1.minute)
     time_2 = today.replace(hour=time_2.hour, minute=time_2.minute)
 
-    between_times_interval(message, doc['contact information'], time_1, time_2, interval, user_id)
+    between_times_interval(message, doc['contact information'], time_1, time_2, interval, user_id, user_tz)
 
     if repeating == 'True':
         # make sure that the call for repeating starts the next day, not today, not two days from now
@@ -396,8 +355,8 @@ async def between_times(ctx, time_1, time_2, interval, message, repeating):
                                          {'$push': {'active jobs': id_}})
 
     await ctx.send(
-        content=f"Message: {message} \nTime: from **{time_1.strftime('%H:%M')}** to **{time_2.strftime('%H:%M')}** "
-                f"Repeating: {repeating}")
+        content=f"⏰ Message: {message} \nTime: from **{time_1.strftime('%H:%M')}** to **{time_2.strftime('%H:%M')}** "
+                f"\nRepeating: {repeating}")
 
 
 # error comes up when the function is placed in the above function, so it's here /shrug
@@ -444,17 +403,15 @@ async def get_schedule(ctx):
             # get jobtype
             jobtrig = f" {str(trig).split('[')[0]}"
 
-            # welcome to the ghetto, getting vars from trigger
+            # welcome to the getting vars from trigger game
             try:
                 if x := short_dt(trig.start_date):
                     jobtrig += f" | **Start time**: {x} "
                 if x := short_dt(trig.end_date):
                     jobtrig += f"**End time**: {x}"
-                jobtrig += f"**Timezone**: {trig.timezone}"
+                jobtrig += f" **Timezone**: {trig.timezone}"
             except AttributeError:
-                from pprint import pprint
-            print(dir(trig))
-
+                pass
 
             next_run_time = format_dt(jobtime)
             str_out += f"*Job: {job_count}* \n" \
@@ -477,8 +434,8 @@ async def remove_schedule(ctx):
         await ctx.send(content=f"Please register your information with 'define-self'")
         return
 
-    temp = db.bot_usage.find_one({'user id': user_id})
-    for value in temp['active jobs']:
+    user_object = db.bot_usage.find_one({'user id': user_id})
+    for value in user_object['active jobs']:
         try:
             mainsched.remove_job(value, 'default')
             db.bot_usage.update_one({'user id': user_id},
@@ -487,9 +444,40 @@ async def remove_schedule(ctx):
             db.bot_usage.update_one({'user id': user_id},
                                     {'$pull': {'active jobs': value}})
 
-    await ctx.send(content="Command executed, listed jobs removed")
+    await ctx.send(content="⏰ Command executed, listed jobs removed")
 
+@slash.slash(name="remove-index", description="remove a single job according to its position", guild_ids=guild_ids,
+             options=[
+                 manage_commands.create_option(
+                     name="index-position",
+                     description="do get-sched to find the job's position",
+                     option_type=4,
+                     required=True
+                 )
+                     ])
+async def remove_index(ctx, index):
+    # verificaiton process
+    try:
+        user_id, id_, doc, user_tz = basic_init(ctx)
+    except TypeError:
+        await ctx.send(content=f"Please register your information with 'define-self'")
+        return
 
+    index -= 1
+
+    try:
+        job_id = db.bot_usage.find_one({'user id': user_id})[f'active jobs'][index]
+    except IndexError:
+        await ctx.send(content=f"**Index not found**")
+        return
+
+    mainsched.remove_job(job_id)
+    db.bot_usage.find_one_and_update({'user id': user_id},
+                                     {'$set': {f'active jobs.{index}': None}})
+    db.bot_usage.find_one_and_update({'user id': user_id},
+                                     {'$pull': {'active jobs': None}})
+
+    await ctx.send(content="⏰ Command executed, listed jobs removed")
 # fun commands
 
 @slash.slash(name="ping", guild_ids=guild_ids)
@@ -497,4 +485,51 @@ async def _ping(ctx):  # Defines a new "context" (ctx) command called "ping."
     await ctx.send(content=f"Pong! ({client.latency * 1000}ms)")
 
 
-client.run("ODAyMzYzNDM1MzM3MjUyODY0.YAuJLg.gC0EWPOtik2ct2jXO5gaNxw66pE")
+def jobitem_removed(event):
+    """
+        the listener checks for when a job is removed or executed and removes it from 'active jobs'
+        """
+
+    user_id = int(event.job_id.split('user')[1])
+    job = mainsched.get_job(event.job_id)
+    try:
+        tz = job.trigger.timezone
+    except AttributeError:
+        # doesn't mattero what the timezone is, just needs to be valid. date_jobs always get removed
+        tz="America/New_York"
+    current_time = datetime.now(timezone(tz))
+
+    try:
+        # if end_date exists, check if end_date has already passed, then remove the job if True. if no end_date,
+        # or end_date not passed, execute the function
+        if x := job.trigger.end_date:
+            if current_time >= x:
+                # the end_date has passed, remove hte job
+                pass
+            else:
+                # the end_date has note yet passed, do not remove the job
+                return
+        else:
+            # there is no end_date, the job never ends
+            return
+    except AttributeError:
+        # this is a date job, proceed as normal remove the job
+        pass
+    db.bot_usage.find_one_and_update({'user id': user_id},
+                                     {'$pull': {'active jobs': event.job_id}})
+    logging.info(f"event: {event}, listener activated")
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    mongoclient = MongoClient(config['MONGO']['mongo_value'])
+    db = mongoclient.discord
+    jobstores = {
+        'default': MongoDBJobStore(client=mongoclient, collection="scheduled-job")
+    }
+    mainsched = AsyncIOScheduler(jobstores=jobstores)
+    mainsched.start()
+
+    mainsched.add_listener(jobitem_removed, EVENT_JOB_EXECUTED | EVENT_JOB_REMOVED)
+    client.run(config["DISCORD"]["bot_token"])
--- utility_file.py
+++ utility_file.py
@@ -6,8 +6,7 @@ def format_dt(dtobject):
     format: Saturday 8:00 PM | Feb 6 2021 ;datetime-->string
     """
     ymd = f"{dtobject.strftime('%b')} {dtobject.day} {dtobject.year}"
-    formatted = f"{dtobject.strftime('%A')} {dtobject.hour % 12}:{dtobject.strftime('%M')} {dtobject.strftime('%p')} | {ymd}"
-
+    formatted = f"{dtobject.strftime('%A')} {dtobject.strftime('%I')}:{dtobject.strftime('%M')} {dtobject.strftime('%p')} | {ymd}"
     return formatted
 
 
@@ -15,7 +14,7 @@ def short_dt(dtobject):
     """
     format: 5:03pm ; datetime--> string
     """
-    formatted = f"{dtobject.hour % 12}:{dtobject.strftime('%M')} {dtobject.strftime('%p')}"
+    formatted = f"{dtobject.strftime('%I')}:{dtobject.strftime('%M')} {dtobject.strftime('%p')}"
     return formatted
 
 
